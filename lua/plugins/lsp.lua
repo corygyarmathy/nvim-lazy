f59748802187d083d38c49b641077da1eea2e385
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-02-24 19:47:33 +0800

    feat(LSP): baseline python LSP configuration

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,39 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      ruff = { -- Python formatter & LSP
+        init_options = {
+          settings = {
+            lint = {
+              enable = false,
+            },
+          },
+        },
+      },
+      basedpyright = { -- Python LSP
+        settings = {
+          basedpyright = {
+            analysis = {
+              diagnosticSeverityOverrides = {
+                -- Categories: "error", "warning", "information", "hint" OR Boolean: true, false
+                reportUnusedImport = "information",
+                reportUnusedVariable = "information",
+                reportUnusedCallResult = "information",
+                -- reportUnusedCallResult = false,
+                -- reportMissingTypeArgument = false,
+                -- reportMissingImports = false,
+                -- reportMissingParameterType = false,
+                -- reportUnknownArgumentType = false,
+                -- reportUnknownLambdaType = false,
+                -- reportUnknownMemberType = false,
+                -- reportUnknownParameterType = false,
+                -- reportUnknownVariableType = false,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
