2df572aba93033c6fbded4dd57968dcc48952a5e
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-02-24 16:37:01 +0800

    feat(snacks): keymaps

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,12 +1,20 @@
 return {
   "folke/snacks.nvim",
   keys = {
+    { "<leader>n", false }, -- Disable default keymap
     {
       "<leader>N",
       function()
         Snacks.picker.notifications()
       end,
       desc = "Notification History",
+    },
+    {
+      "\\",
+      function()
+        Snacks.explorer()
+      end,
+      desc = "Explorer Snacks (root dir)",
     },
   },
 }
