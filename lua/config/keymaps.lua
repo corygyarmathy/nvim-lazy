d64a9bf70f3178a9da246ab6f94c0ad5913fde5a
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-03-08 14:38:37 +0800

    feat: set <C-u> and <C-d> to centre after moving with zz

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Make page up and down centred
+map("n", "<C-u>", "<C-u>zz", { desc = "Move up half page" })
+map("n", "<C-d>", "<C-d>zz", { desc = "Move down half page" })
