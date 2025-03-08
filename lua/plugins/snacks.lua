38ce7a46dcf0532a9740b3b4532d57c1cab0eeeb
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-03-08 14:39:19 +0800

    chore(snacks): remove commented-out keys config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,22 +1,5 @@
 return {
   "folke/snacks.nvim",
-  keys = {
-    -- { "<leader>n", false }, -- Disable default keymap
-    -- { "<leader>e", false }, -- Disable default keymap
-    -- {
-    --   "<leader>N",
-    --   function()
-    --     Snacks.picker.notifications()
-    --   end,
-    --   desc = "Notification History",
-    -- },
-    -- {
-    --   "\\",
-    --   function()
-    --     Snacks.explorer()
-    --   end,
-    --   desc = "Explorer Snacks (root dir)",
-    -- },
   opts = {
     scroll = {
       enabled = false,
