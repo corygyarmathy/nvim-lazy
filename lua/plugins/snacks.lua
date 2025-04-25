0fa826c34e87b3dc0014c8c6c9bcb78bb6052e7c
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-04-25 15:57:06 +0800

    feat: show hidden and ignored files in snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,6 +1,17 @@
 return {
   "folke/snacks.nvim",
+  ---@type snacks.Config
   opts = {
+    picker = {
+      hidden = true,
+      ignored = true,
+      sources = {
+        files = {
+          hidden = true,
+          -- ignored = true,
+        },
+      },
+    },
     scroll = {
       enabled = false,
     },
